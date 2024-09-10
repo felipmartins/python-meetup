83810a2a885721491874a32b77c57f8ac3984715
felipmartins <felipmartins94@gmail.com>
2024-09-10 18:16:25 -0300

    fix test that was failing

--- tests/routes/test_index_routes.py
+++ tests/routes/test_index_routes.py
@@ -9,7 +9,7 @@ client = TestClient(app)
 def test_home_route():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Welcome Python MeetUp 2023!!"}
+    assert response.json() == {"message": "Welcome Python MeetUp 2024!!"}
 
 
 def test_health_route():
